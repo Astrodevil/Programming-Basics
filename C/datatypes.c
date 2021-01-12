89ad2b9a8f8e92275f21d918723d4ea3976e76d9
Astrodevil <>
2021-01-12 23:29:26 +0530

    New files Added

---
+++ C/datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 40;
+    double gpa = 8.8;
+    char grade = 'A';
+    char phrase[] = "Astrodevil";
+
+    return 0;
+}
---
+++ C/workingw8nos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    printf ("%f", 4.6);
+
+    return 0;
+}
