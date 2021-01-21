a166bce9fc18d59766b7d0ce58e06f3e5e74df2d
Astrodevil <>
2021-01-21 18:10:25 +0530

    Create GettingUserInput.c

---
+++ C/GettingUserInput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char grade;
+    printf("Enter your grade:");
+    scanf("%c", &grade);
+    printf("You grade is %c",grade);
+
+    return 0;
+}
