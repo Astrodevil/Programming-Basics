ecbce4f574459f3038af239dec13ffa4f18f3ab9
Astrodevil <>
2021-02-26 19:57:15 +0530

    updated

---
+++ C++/calc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter first number:\n";
+    cin>>num1;
+
+    cout<<"Enter second number:\n";
+    cin>>num2;
+
+    cout<<"The Addition is"<<num1 + num2;
+    cout<<"The Substraction is"<<num1 - num2;
+    cout<<"The Multiplication is"<<num1 * num2;
+    cout<<"The division is"<<num1 / num2;
+    
+return 0;
+}
