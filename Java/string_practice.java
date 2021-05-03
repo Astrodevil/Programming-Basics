559263e1c81a623e800ec0c7cd54570ecb054275
Shivam <skg050210@gmail.com>
2021-05-03 21:25:14 +0530

    New concepts added

---
+++ Java/string_practice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class string_practice {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String s = sc.nextLine();
+        //System.out.println(s.replace(" ","_"));
+        //System.out.println("Dear "+s+", Thanks a lot");
+        //System.out.println(s.indexOf("  "));
+        //System.out.println(s.indexOf("   "));
+        String myLetter = "Dear Shivam,\n\tThis Java Course is Nice.\nThanks!";
+        System.out.println(myLetter);
+
+    }
+}
