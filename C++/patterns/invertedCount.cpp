2757e0e76afc9df6756ceb4e23ca022ad08662f5
AAYUSH SHARMA <aayushsharma0909@gmail.com>
2021-09-28 22:30:02 +0530

    Added patterns code in C++

---
+++ C++/patterns/invertedCount.cpp
@@ -0,0 +1,23 @@
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
