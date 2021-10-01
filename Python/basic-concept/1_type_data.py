84444c1ea444cc7d2ed14d278180c3eb790b5934
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-10-01 09:33:30 +0700

    create module of basic python

---
+++ Python/basic-concept/1_type_data.py
@@ -0,0 +1,44 @@
+"""
+author: https://github.com/alif-arrizqy
+lesson: type data in python
+"""
+
+# text = str
+# numeric = int, float
+# sequence = list, tuple, range
+# mapping = dict
+# boolean = bool
+
+# str
+x = "Hello World"
+print(x)
+
+# int 
+x = 12
+print(x)
+
+# float
+x = 21.2
+print(x)
+
+# list
+x = ["apple", "banana", "watermelon"]
+print(x)
+
+# tuple
+x = ("apple", "banana", "watermelon")
+print(x)
+
+# range
+x = range(4)
+print(x)
+
+# dict
+x = {"name": "alif", "age": 22}
+print(x)
+
+# boolean
+x = True
+print(x)
+y = False
+print(y)
---
+++ Python/basic-concept/6_while_loop.py
@@ -0,0 +1,9 @@
+"""
+author: https://github.com/alif-arrizqy
+lesson: while loops in python
+With the while loop we can execute a set of statements as long as a condition is true.
+"""
+
+x = 8
+while x < 12:
+    print(x)
