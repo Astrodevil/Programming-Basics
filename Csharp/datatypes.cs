9ad9a53e517a5b9b0916524009e21ca6b992b12b
Nash Boisvert <fenderfan@live.ca>
2021-10-20 13:49:54 -0600

    - added a few basics

--- Csharp/datatypes.cs
+++ Csharp/datatypes.cs
@@ -1,1 +1,28 @@
-ECHO is on.
+using System;
+
+namespace examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In this page we will cover some different data types you will find in C#
+            
+            //Variable declaration is similar to other languages, but we need to declare data types.
+
+            string name = "Nash"; //string of characters, double quotes
+
+            char letter = 'a';  //Single letter, single quotes
+
+            bool valid = true;//false
+
+            int number = 1; //WHOLE numbers +- -2,147,483,648 to 2,147,483,647
+            
+            long bigNumber; //Stores large numbers, +- 9,223,372,036,854,775,808
+            
+            float smallFraction; //FRACTIONAL 6 - 7 decimal points.
+            
+            double fraction = 1.5; //FRACTIONAL up to 15 decimal points.
+        }
+    }
+}
--- Csharp/helloworld.cs
+++ Csharp/helloworld.cs
@@ -6,7 +6,14 @@ namespace examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //This is a basic example of hello world and the boiler plate for C#
+            Console.WriteLine("Hello World!"); 
+            //Console.WriteLine <-- prints to console on new line. 
+            
+            Console.Write("Hello");
+            Console.Write("World!");
+            //Console.Write will also print to console, but not on a new line.
+            // This will still out put "Hello World!"
         }
     }
 }
