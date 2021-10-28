b3d36c849da31ff2caba735fff2185dc7aee15ad
Vipul Kumar Singh <Vipulrjput@gmail.com>
2021-10-28 22:33:26 +0530

    file reallocated

--- DSA/DSA-CPP/Matrix/spiralmatrix.cpp
+++ DSA/DSA-CPP/Matrix/spiralmatrix.cpp
@@ -1,3 +1,7 @@
+/*
+link :: https://leetcode.com/problems/spiral-matrix/
+*/
+
 class Solution
 {
 public:
@@ -54,3 +58,22 @@ public:
         return spiral ; 
     }
 };
+
+/*
+
+Constraints ::
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+
+Example ::
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+*/
