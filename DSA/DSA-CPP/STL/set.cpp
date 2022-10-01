23d2eac6835a02b69972d84df6a62b85473347b2
Aman Hussain <73658735+Panic100@users.noreply.github.com>
2022-10-02 00:04:00 +0530

    Set in Cpp
    Completed the code it was missing

--- DSA/DSA-CPP/STL/set.cpp
+++ DSA/DSA-CPP/STL/set.cpp
@@ -1,3 +1,19 @@
 //tales only unique value 
 // set returns value in sorted order
 //unordered set returns in random order
+#include<iostream>
+#include<set>
+using namespace std;
+int main() {
+	set<int>s;
+	s.insert(1);
+	s.insert(1);
+	s.insert(3);
+	s.insert(3);
+	s.insert(2);
+	s.insert(0);
+	for (auto &i : s) {
+		cout << i << " ";
+	}
+	return 0;
+}
