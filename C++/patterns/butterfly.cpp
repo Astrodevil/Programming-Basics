fdc3bab4dc9a8658cf5962847230a4944f759d11
Aman Singh <73817213+aman-7299@users.noreply.github.com>
2022-10-30 23:30:05 +0530

    Update butterfly.cpp

--- C++/patterns/butterfly.cpp
+++ C++/patterns/butterfly.cpp
@@ -1,56 +1,32 @@
-// $        $
-// $$      $$
-// $$$    $$$
-// $$$$  $$$$
-// $$$$$$$$$$
-// $$$$$$$$$$
-// $$$$  $$$$
-// $$$    $$$
-// $        $
 
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-typedef long long ll;
 
-int main(void)
+int main()
 {
-    int n;
-    cin >> n;
-    for (int i = n - 1; i >= 0; i--)
-    {
-        for (int j = n - 1; j >= i; j--)
-        {
-            cout << "$";
-        }
-        for (int j = 0; j < 2 * i; j++)
-        {
-            cout << " ";
-        }
-        for (int j = n - 1; j >= i; j--)
-        {
-            cout << "$";
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = n - 1; j >= i; j--)
-        {
-            cout << "$";
-        }
-        for (int j = 0; j < 2 * i; j++)
-        {
-            cout << " ";
-        }
-        for (int j = n - 1; j >= i; j--)
-        {
-            cout << "$";
-        }
-        cout << endl;
-    }
+  int r,i,j;
+  cout<<"Enter no. of rows: ";
+  cin>>r;
+  
+  for(i=1;i<=r;i++)
+  {
+    for(j=1;j<=i;j++)
+    cout<<"*";
+    for(j=1;j<=2*(r-i);j++)
+    cout<<" ";
+    for(j=1;j<=i;j++)
+    cout<<"*";
+    cout<<"\n";
+  }
+    for(i=r;i>=1;i--)
+  {
+    for(j=1;j<=i;j++)
+    cout<<"*";
+    for(j=1;j<=2*(r-i);j++)
+    cout<<" ";
+    for(j=1;j<=i;j++)
+    cout<<"*";
+    cout<<"\n";
+  }
+  return 0;
 }
-
-// 0123
-// @  @ 0
-// @@
-// @@@@@@ 1
